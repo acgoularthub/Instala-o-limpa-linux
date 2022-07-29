fc3d4ece574b3077a5b34910b4cd66409a8dbeb2
Anderson Coelho Goulart <acgoulartmail@gmail.com>
2022-07-29 18:13:31 -0300

    Incrementada novas funcionalidades no script

--- newInstalation.sh
+++ newInstalation.sh
@@ -20,6 +20,7 @@ PROGRAMAS_PARA_INSTALAR=(
   gnome-tweaks
   gnome-tweak-tool
   flatpak
+  gnome-software-plugin-flatpak
   python3
   python3-pip
   python3-venv
@@ -38,11 +39,73 @@ for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
   fi
 done
 
-## Instalando pacotes Flatpak ##
-flatpak install flathub com.obsproject.Studio -y
+echo " "
+echo "--------------------------------------------------------------------------------"
+echo " "
 
+# Instalando docker e kubernetes
+
+echo " "
+echo "Iniciando a instalação do Docker"
+echo " "
+
+#docker
+curl -fsSL https://get.docker.com/ | sh
+sudo usermod -aG docker $USER
+newgrp docker # Entra no grupo docker
+sudo systemctl enable docker.service
+
+echo " "
+echo "---------------------------------------------------------------------------------"
+echo " "
+
+echo " "
+echo "Instalando o Kubectl"
+echo " "
+
+#kubernetes
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo " "
+echo "---------------------------------------------------------------------------------"
+echo " "
+
+echo " "
+echo "Instalando o Kind"
+echo " "
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+echo " "
+echo "---------------------------------------------------------------------------------"
+echo " "
+
+echo " "
+echo "Instalando flatpaks"
+echo " "
+
+## Instalando pacotes Flatpak ##
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.stremio.Stremio -y
+
+echo " "
+echo "---------------------------------------------------------------------------------"
+echo " "
+
+echo " "
+echo "Instalando snaps"
+echo " "
 ## Instalando pacotes Snap ##
 sudo snap install spotify
+
+echo " "
+echo "Finalizando"
+echo " "
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza##
@@ -51,3 +114,7 @@ flatpak update
 sudo apt autoclean
 sudo apt autoremove -y
 # ---------------------------------------------------------------------- #
+
+echo " "
+echo "***--------------FIM DO SCRIPT----------------***"
+echo " "
